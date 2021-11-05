07b0f856536dab565e8103fbf3841afca64ed750
besanz <benat.sanz@opendeusto.es>
2021-11-05 20:09:34 +0100

    DB: A bit of Utils & First test

---
+++ src/Test/DBConnectorTest.java
@@ -0,0 +1,16 @@
+package Test;
+
+import DB.DBConnector;
+import junit.framework.TestCase;
+
+public class DBConnectorTest extends TestCase {
+    private static DBConnector connector;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
