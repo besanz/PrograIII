8ea6854a1db81bd9c2571b9417dd0bc2e64ecd4d
besanz <benat.sanz@opendeusto.es>
2021-11-08 21:02:48 +0100

    Empty Test + HOME PC check

---
+++ src/test/BasketTest.java
@@ -0,0 +1,69 @@
+package test;
+
+import data.Basket;
+import org.junit.*;
+import org.junit.Test;
+
+public class BasketTest {
+
+        Basket basket;
+
+        @Before
+        public void setUp(){
+            basket = new Basket();
+        }
+
+        @Test
+        public void addOneItemToBasket() {
+
+        }
+
+        @Test
+        public void addTwoItemsToBasket(){
+
+        }
+
+        @Test
+        public void addAnItemTwice(){
+
+        }
+
+        @Test
+        public void checkOutOneItem(){
+
+        }
+
+        @Test
+        public void checkoutTwoSeparateItems(){
+
+        }
+
+        @Test
+        public void checkoutThreeSeparateItems(){
+
+
+        }
+
+        @Test
+        public void checkoutTwoSameItems(){
+
+
+        }
+
+        @Test
+        public void checkoutThreeItems(){
+
+
+        }
+
+        @Test
+        public void checkoutManyItems(){
+
+
+        }
+
+        @Test
+        public void checkOutAnInvalidItem(){
+
+        }
+}
