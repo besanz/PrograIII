6eb82288abcb9941f6e729583e38a1bd590f56ba
Jon Ander <jonander.aranaopendeusto.es>
2021-11-10 11:23:01 +0100

    Windows added

--- src/windows/Basket.java
+++ src/windows/Basket.java
@@ -1,64 +1,73 @@
 package windows;
 
-import data.Product;
-import data.User;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTable;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class Basket extends JDialog {
+/**
+ * 
+ * @author Benat;
+ *
+ */
 
+public class Basket extends JFrame {
 
+	private JPanel contentPane;
+	private JTable table;
 
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Basket frame = new Basket();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
-
+	/**
+	 * Create the frame.
+	 */
+	public Basket() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 597);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		table = new JTable();
+		table.setBounds(74, 103, 271, 306);
+		contentPane.add(table);
+		
+		JButton btnRemove = new JButton("Remove");
+		btnRemove.setBounds(218, 425, 127, 29);
+		contentPane.add(btnRemove);
+		
+		JButton btnBack = new JButton("Back");
+		btnBack.setBounds(74, 425, 129, 29);
+		contentPane.add(btnBack);
+		
+		JButton btnBuy = new JButton("Buy Now");
+		btnBuy.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnBuy.setBounds(74, 470, 271, 29);
+		contentPane.add(btnBuy);
+	}
 
 }
-
-
-
-    /**
-     * @Author Beñat
-     *
-     * This method adds a product at the current basket, while showing real-time total number of products in it,
-     * so it may as well change that value.
-     *
-     * NOT FINISHED!
-     *
-     * @throws BasketException
-     */
-/*
-    public void addProducts(final Product product) throws BasketException {
-        ArrayList<Product> products = new ArrayList<Product>();
-        if (product != null) {
-            int currentAmount = this.products.size();
-            currentAmount += currentAmount;
-            this.products.add(product);
-        } else {
-            throw new BasketException("You cannot add Null objects to Basket!");
-        }
-    }
-*/
-    /**
-     * @author Beñat
-     *
-     * This method removes a product from the current basket, while showing real-time total number of products in it,
-     * so it may as well change that value.
-     *
-     * NOT FINISHED!
-     *
-     * @param product
-     * @throws BasketException
-     */
-
-/*
-    public void removeProducts(final Product product) throws BasketException {
-        if (product != null) {
-            int currentAmount = this.products.size();
-            currentAmount += currentAmount;
-        } else {
-            throw new BasketException("Cannot remove %d instances of product as there are only %d instances!");
-        }
-    }
-*/
-
-
