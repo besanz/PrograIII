242754bd7033cc3648ebb7122566c7250951ab11
besanz <benat.sanz@opendeusto.es>
2021-12-02 20:30:48 +0100

    Main window buttons

--- src/windows/MainWindow.java
+++ src/windows/MainWindow.java
@@ -43,28 +43,29 @@ public class MainWindow extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 674, 571);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.ORANGE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		
+
+
 
 		ArrayList<Product> selectProduct = db.SelectProduct.selectProduct(sql);
 		DefaultListModel<Product>model=new DefaultListModel<Product>();
 
 
 		JComboBox orderBy = new JComboBox();
-		orderBy.setBounds(447, 43, 138, 26);
+		orderBy.setBounds(462, 43, 138, 26);
 		contentPane.add(orderBy);
 
 		txtFilter = new JTextField();
 		txtFilter.setBackground(Color.LIGHT_GRAY);
-		txtFilter.setBounds(67, 43, 131, 26);
+		txtFilter.setBounds(15, 119, 131, 26);
 		contentPane.add(txtFilter);
 		txtFilter.setColumns(10);
 
 		JButton btnSearch = new JButton("Search");
-		btnSearch.setBounds(213, 40, 79, 29);
+		btnSearch.setBounds(161, 118, 79, 29);
 		contentPane.add(btnSearch);
 
 		JButton btnBasket = new JButton("Basket");
@@ -77,25 +78,20 @@ public class MainWindow extends JFrame {
 			}
 		});
 
-
-		JLabel lblSelectedElements = new JLabel("New label");
-		lblSelectedElements.setBounds(384, 452, 86, 20);
-		contentPane.add(lblSelectedElements);
-		
 		DefaultListModel<Product>model1=new DefaultListModel<Product>();
 		for(Product a: selectProduct)
 		{
-		model1.addElement(a);
+			model1.addElement(a);
 		}
 		JList list = new JList();
 		list.setModel(model1);
 		list.setBounds(15, 161, 432, 216);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(15, 161, 432, 216);
 		scrollPane.setViewportView(list);
 		contentPane.add(scrollPane);
-		
+
 		JButton btnInformation = new JButton("Information");
 		btnInformation.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -107,14 +103,47 @@ public class MainWindow extends JFrame {
 		});
 		btnInformation.setBounds(462, 159, 138, 29);
 		contentPane.add(btnInformation);
-		
-		
-		
-		
+
+		JButton btnNewProduct = new JButton("New Product");
+		btnNewProduct.setBounds(502, 244, -17, -27);
+		contentPane.add(btnNewProduct);
+
+		JButton btnNewProduct_1 = new JButton("New Product");
+		btnNewProduct_1.setBounds(462, 204, 138, 29);
+		contentPane.add(btnNewProduct_1);
+
+		JButton btnNewProduct_1_1 = new JButton("View Users");
+		btnNewProduct_1_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewProduct_1_1.setBounds(462, 244, 138, 29);
+		contentPane.add(btnNewProduct_1_1);
+
+		JButton btnNewProduct_1_1_1 = new JButton("Settings");
+		btnNewProduct_1_1_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewProduct_1_1_1.setBounds(462, 287, 138, 29);
+		contentPane.add(btnNewProduct_1_1_1);
+
+		JButton btnNewButton = new JButton("Back");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(462, 448, 138, 29);
+		contentPane.add(btnNewButton);
+
+
+
+
 	}
 	String sql = "Selecct id,name,Price,stock from Product ";
 
 	DefaultTableModel  tbl = new DefaultTableModel();
 
 	ArrayList<Product> productArrayList= SelectProduct.selectProduct(sql);
+
 }
