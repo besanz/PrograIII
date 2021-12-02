3a517b71a7730401ddd6f3a04652741354e5068b
besanz <benat.sanz@opendeusto.es>
2021-12-02 17:11:43 +0100

    Initialization Fix 2.0

--- src/app/Initialization.java
+++ src/app/Initialization.java
@@ -11,15 +11,9 @@ import java.awt.*;
 public class Initialization {
 
     public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
+
                     Login window = new Login();
                     window.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+
     }
 }
