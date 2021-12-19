0960332ccb7f6a81b4e36daf27ead866981d67b8
besanz <benat.sanz@opendeusto.es>
2021-12-19 14:38:56 +0100

    App & JavaDoc

--- src/app/Initialization.java
+++ src/app/Initialization.java
@@ -3,12 +3,14 @@ package app;
 import windows.Login;
 
 /**
+ * @author Benat;
  * Initialize app
  */
 
 public class Initialization {
 
     public static void main(String[] args) {
+    	//Will start app here.
                     Login window = new Login(null);
                     window.setVisible(true);
     }
